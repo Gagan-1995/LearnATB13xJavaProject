a308a264efffdd4f6ffeb456b95bf7d4095d2827
Gagan-1995 <gaganashoka@gmail.com>
2025-06-05 08:55:57 +0530

    Initial commit

---
+++ src/ex_01_JavaBasic/Lab001_HelloWorldProgram.java
@@ -0,0 +1,9 @@
+package ex_01_JavaBasic;
+
+import java.util.Arrays;
+
+public class Lab001_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Training");
+    }
+}
